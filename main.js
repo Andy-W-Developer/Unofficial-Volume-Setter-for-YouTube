c5e4718342282f85d7d62f6ca50bb8fd6ed30418
unknown <128531452+Andy-W-Developer@users.noreply.github.com>
2023-04-01 14:58:34 +1300

    Add code to parse volumes

--- main.js
+++ main.js
@@ -11,3 +11,12 @@ var contextMenuItems = document.getElementsByClassName("ytp-menuitem")
 var click = new MouseEvent("click")
 
 contextMenuItems[6].dispatchEvent(click)
+
+var infoPanel = document.getElementsByClassName("html5-video-info-panel-content ytp-sfn-content")[0]
+
+var infoPanelVolumes = infoPanel.getElementsByTagName("span")[3].textContent.split(' ')
+var volumeNormalized = parseInt(infoPanelVolumes[3].replace('%', ''))
+var volumeDecibel
+if (infoPanelVolumes[6].includes('-')) {
+    volumeDecibel = parseInt(infoPanelVolumes[6].replace('-', '').replace('dB)',''))
+}
