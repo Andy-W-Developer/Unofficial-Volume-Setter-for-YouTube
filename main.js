a27127ad1d034a7a191cb9d84baf7ac0ee881f67
unknown <128531452+Andy-W-Developer@users.noreply.github.com>
2023-04-03 13:14:29 +1200

    Add semicolons

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-var video = document.getElementById("movie_player")
-var menu = new MouseEvent("contextmenu")
+var video = document.getElementById("movie_player");
+var menu = new MouseEvent("contextmenu");
 
-video.dispatchEvent(menu)
+video.dispatchEvent(menu);
 
-var menuItems = document.getElementsByClassName("ytp-menuitem")
-var click = new MouseEvent("click")
+var menuItems = document.getElementsByClassName("ytp-menuitem");
+var click = new MouseEvent("click");
 
-menuItems[6].dispatchEvent(click)
+menuItems[6].dispatchEvent(click);
 
-var infoPanel = document.getElementsByClassName("html5-video-info-panel-content ytp-sfn-content")[0]
+var infoPanel = document.getElementsByClassName("html5-video-info-panel-content ytp-sfn-content")[0];
 
 // EXAMPLE: Array(7) [ "", "100%", "/", "100%", "(content", "loudness", "-1.7dB)" ]
-var infoPanelVolumes = infoPanel.getElementsByTagName("span")[3].textContent.split(' ')
-var volumeNormalized = parseInt(infoPanelVolumes[3].replace('%', ''))
+var infoPanelVolumes = infoPanel.getElementsByTagName("span")[3].textContent.split(' ');
+var volumeNormalized = parseInt(infoPanelVolumes[3].replace('%', ''));
 var volumeDecibel
 if (infoPanelVolumes[6].includes('-')) {
-    volumeDecibel = parseFloat(infoPanelVolumes[6].replace('-', '').replace('dB)',''))
+    volumeDecibel = parseFloat(infoPanelVolumes[6].replace('-', '').replace('dB)',''));
 }
 
-var infoPanelClose = document.getElementsByClassName("ytp-sfn-close html5-video-info-panel-close ytp-button")[0]
+var infoPanelClose = document.getElementsByClassName("ytp-sfn-close html5-video-info-panel-close ytp-button")[0];
 
-infoPanelClose.dispatchEvent(click)
+infoPanelClose.dispatchEvent(click);
