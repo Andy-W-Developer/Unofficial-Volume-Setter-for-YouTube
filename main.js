fbfe5a34eda1bcb74cef3911dcd598e9d6160d07
unknown <128531452+Andy-W-Developer@users.noreply.github.com>
2023-04-03 12:52:34 +1200

    Change spacing and variable names to be shorter

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 var video = document.getElementById("movie_player")
-var contextMenu = new MouseEvent("contextmenu")
+var menu = new MouseEvent("contextmenu")
+video.dispatchEvent(menu)
 
-video.dispatchEvent(contextMenu)
-
-var contextMenuItems = document.getElementsByClassName("ytp-menuitem")
+var menuItems = document.getElementsByClassName("ytp-menuitem")
 var click = new MouseEvent("click")
-
-contextMenuItems[6].dispatchEvent(click)
+menuItems[6].dispatchEvent(click)
 
 var infoPanel = document.getElementsByClassName("html5-video-info-panel-content ytp-sfn-content")[0]
-
 var infoPanelVolumes = infoPanel.getElementsByTagName("span")[3].textContent.split(' ')
+
 var volumeNormalized = parseInt(infoPanelVolumes[3].replace('%', ''))
 var volumeDecibel
 if (infoPanelVolumes[6].includes('-')) {
