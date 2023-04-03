21322889944bda42bfd77aa026433d29de287d5a
unknown <128531452+Andy-W-Developer@users.noreply.github.com>
2023-04-03 13:01:47 +1200

    Add an example of the volumes array after splitting and change spacing

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 var video = document.getElementById("movie_player")
 var menu = new MouseEvent("contextmenu")
+
 video.dispatchEvent(menu)
 
 var menuItems = document.getElementsByClassName("ytp-menuitem")
 var click = new MouseEvent("click")
+
 menuItems[6].dispatchEvent(click)
 
 var infoPanel = document.getElementsByClassName("html5-video-info-panel-content ytp-sfn-content")[0]
-var infoPanelVolumes = infoPanel.getElementsByTagName("span")[3].textContent.split(' ')
 
+// EXAMPLE: Array(7) [ "", "100%", "/", "100%", "(content", "loudness", "-1.7dB)" ]
+var infoPanelVolumes = infoPanel.getElementsByTagName("span")[3].textContent.split(' ')
 var volumeNormalized = parseInt(infoPanelVolumes[3].replace('%', ''))
 var volumeDecibel
 if (infoPanelVolumes[6].includes('-')) {
@@ -16,4 +19,5 @@ if (infoPanelVolumes[6].includes('-')) {
 }
 
 var infoPanelClose = document.getElementsByClassName("ytp-sfn-close html5-video-info-panel-close ytp-button")[0]
+
 infoPanelClose.dispatchEvent(click)
