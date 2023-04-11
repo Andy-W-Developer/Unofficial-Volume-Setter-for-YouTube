931ebd3aa1806bb14de127832be0134e3f108e9a
unknown <128531452+Andy-W-Developer@users.noreply.github.com>
2023-04-11 17:08:43 +1200

    Add code to select the close button for the Stats for nerds panel

--- src/modules/panels.js
+++ src/modules/panels.js
@@ -3,3 +3,5 @@ var menu = new MouseEvent("contextmenu");
 
 var menuItems = document.getElementsByClassName("ytp-menuitem");
 var click = new MouseEvent("click");
+
+var infoPanelClose = document.getElementsByClassName("ytp-sfn-close html5-video-info-panel-close ytp-button")[0];
