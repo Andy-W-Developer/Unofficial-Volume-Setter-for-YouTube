a0298c1e2c826b9cc447f333baa527ece3d681e7
unknown <128531452+Andy-W-Developer@users.noreply.github.com>
2023-04-11 17:07:52 +1200

    Remove event dispatches from panel.js module

--- src/modules/panels.js
+++ src/modules/panels.js
@@ -1,9 +1,5 @@
 var video = document.getElementById("movie_player");
 var menu = new MouseEvent("contextmenu");
 
-video.dispatchEvent(menu);
-
 var menuItems = document.getElementsByClassName("ytp-menuitem");
 var click = new MouseEvent("click");
-
-menuItems[6].dispatchEvent(click);
