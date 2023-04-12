d2fcbda7a4672f439156f2db78f26e614d1f688a
unknown <128531452+Andy-W-Developer@users.noreply.github.com>
2023-04-12 20:07:04 +1200

    Add unfinished functions

--- src/modules/panels.mjs
+++ src/modules/panels.mjs
@@ -5,3 +5,17 @@ var menuItems = document.getElementsByClassName("ytp-menuitem");
 var click = new MouseEvent("click");
 
 var infoPanelClose = document.getElementsByClassName("ytp-sfn-close html5-video-info-panel-close ytp-button")[0];
+
+function videoPanelOpen() {
+
+}
+
+function videoPanelSelect(option) {
+
+}
+
+function videoInfoPanelClose() {
+
+}
+
+export {videoPanelOpen, videoPanelSelect, videoInfoPanelClose}
