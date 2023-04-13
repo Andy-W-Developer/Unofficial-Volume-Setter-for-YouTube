24322329d25f5b5d446b6cf827e8feaa4e2abaee
unknown <128531452+Andy-W-Developer@users.noreply.github.com>
2023-04-13 18:43:42 +1200

    Changed panel.mjs partial update

--- src/modules/panels.mjs
+++ src/modules/panels.mjs
@@ -1,21 +1,26 @@
-var video = document.getElementById("movie_player");
-var menu = new MouseEvent("contextmenu");
-
-var menuItems = document.getElementsByClassName("ytp-menuitem");
-var click = new MouseEvent("click");
-
-var infoPanelClose = document.getElementsByClassName("ytp-sfn-close html5-video-info-panel-close ytp-button")[0];
-
 function videoPanelOpen() {
+    var video = document.getElementById("movie_player");
+    var menu = new MouseEvent("contextmenu");
 
+    video.dispatchEvent(menu);
 }
 
-function videoPanelSelect(option) {
+function videoPanelSelectIndex(index) {
+    var menuItems = document.getElementsByClassName("ytp-menuitem");
+    var click = new MouseEvent("click");
+
+    menuItems[index].dispatchEvent(click);
+}
+
+function videoPanelSelectString(option) {
 
 }
 
 function videoInfoPanelClose() {
-
+    var infoPanelClose = document.getElementsByClassName("ytp-sfn-close html5-video-info-panel-close ytp-button")[0];
 }
 
-export {videoPanelOpen, videoPanelSelect, videoInfoPanelClose}
+export {videoPanelOpen,
+        videoPanelSelectIndex,
+        videoPanelSelectString,
+        videoInfoPanelClose}
