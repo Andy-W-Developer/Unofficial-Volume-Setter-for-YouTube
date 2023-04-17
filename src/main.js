cd96f63252929ee28e2d8886264544f64a2defc4
unknown <128531452+Andy-W-Developer@users.noreply.github.com>
2023-04-17 21:48:03 +1200

    Replace code in main.js with imported functions

--- src/main.js
+++ src/main.js
@@ -1,32 +1,12 @@
-import {videoPanelOpen, videoPanelSelectIndex, videoInfoPanelClose} from "./modules/panels.mjs"
+import * as panels from "/modules/panels.mjs";
+import * as parse from "/modules/parse.mjs";
+import * as volume from "/modules/volume.mjs";
 
-videoPanelOpen();
+panels.videoPanelOpen();
+panels.videoPanelSelect(6);
 
-videoPanelSelectIndex(6);
+volumeDecibel = parse.parseVolumes();
 
-var infoPanel = document.getElementsByClassName("html5-video-info-panel-content ytp-sfn-content")[0];
+panels.videoInfoPanelClose();
 
-// EXAMPLE: Array(7) [ "", "100%", "/", "100%", "(content", "loudness", "-1.7dB)" ]
-var infoPanelVolumes = infoPanel.getElementsByTagName("span")[3].textContent.split(' ');
-var volumeDecibel
-if (infoPanelVolumes[6].includes('-')) {
-    volumeDecibel = parseFloat(infoPanelVolumes[6].replace('dB)',''));
-    if (volumeDecibel < -15) {
-        volumeDecibel = -15;
-    }
-
-    const audioContext = new AudioContext();
-    const audio = document.getElementsByClassName("video-stream html5-main-video")[0];
-
-    const track = audioContext.createMediaElementSource(audio);
-    var audioGain = audioContext.createGain();
-
-    volumeRatio = 10 ** (volumeDecibel / 20);
-    volumeGain = 1 / volumeRatio;
-
-    audioGain.gain.value = volumeGain;
-
-    track.connect(audioGain).connect(audioContext.destination);
-}
-
-videoInfoPanelClose();
+volume.volumeChange(volumeDecibel);
