3d3e43ceb88128c9341d7a43ea0f83161a2342e9
unknown <128531452+Andy-W-Developer@users.noreply.github.com>
2023-04-29 11:41:42 +1200

    Remove modules and move everything into main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,39 @@
-import * as panels from "/modules/panels.mjs";
-import * as parse from "/modules/parse.mjs";
-import * as volume from "/modules/volume.mjs";
+var video = document.getElementById("movie_player");
+var menu = new MouseEvent("contextmenu");
 
-panels.videoPanelOpen();
-panels.videoPanelSelect(6);
+video.dispatchEvent(menu);
 
-volumeDecibel = parse.parseVolumes();
+var menuItems = document.getElementsByClassName("ytp-menuitem");
+var click = new MouseEvent("click");
 
-panels.videoInfoPanelClose();
+menuItems[6].dispatchEvent(click);
 
-volume.volumeChange(volumeDecibel);
+var infoPanel = document.getElementsByClassName("html5-video-info-panel-content ytp-sfn-content")[0];
+
+// EXAMPLE: Array(7) [ "", "100%", "/", "100%", "(content", "loudness", "-1.7dB)" ]
+var infoPanelVolumes = infoPanel.getElementsByTagName("span")[3].textContent.split(' ');
+var volumeDecibel
+if (infoPanelVolumes[6].includes('-')) {
+    volumeDecibel = parseFloat(infoPanelVolumes[6].replace('dB)',''));
+    if (volumeDecibel < -8) {
+        volumeDecibel = -8;
+    }
+}
+
+const audioContext = new AudioContext();
+const audio = document.getElementsByClassName("video-stream html5-main-video")[0];
+
+const track = audioContext.createMediaElementSource(audio);
+var audioGain = audioContext.createGain();
+
+volumeRatio = 10 ** (volumeDecibel / 20);
+volumeGain = 1 / volumeRatio;
+
+audioGain.gain.value = volumeGain;
+
+track.connect(audioGain).connect(audioContext.destination);
+
+var infoPanelClose = document.getElementsByClassName("ytp-sfn-close html5-video-info-panel-close ytp-button")[0];
+var click = new MouseEvent("click");
+
+infoPanelClose.dispatchEvent(click);
