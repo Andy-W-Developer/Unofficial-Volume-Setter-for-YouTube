9fa897c14e956b178f88b249d0ca89a148a676f5
unknown <128531452+Andy-W-Developer@users.noreply.github.com>
2023-04-29 12:27:02 +1200

    Update code to have a target volume for all videos and add comments for each section

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+// *Open the Stats for nerds panel
 var video = document.getElementById("movie_player");
 var menu = new MouseEvent("contextmenu");
 
@@ -8,17 +9,25 @@ var click = new MouseEvent("click");
 
 menuItems[6].dispatchEvent(click);
 
-var infoPanel = document.getElementsByClassName("html5-video-info-panel-content ytp-sfn-content")[0];
-
+// *Parse volumes
 // EXAMPLE: Array(7) [ "", "100%", "/", "100%", "(content", "loudness", "-1.7dB)" ]
+var infoPanel = document.getElementsByClassName("html5-video-info-panel-content ytp-sfn-content")[0];
 var infoPanelVolumes = infoPanel.getElementsByTagName("span")[3].textContent.split(' ');
-var volumeDecibel
-if (infoPanelVolumes[6].includes('-')) {
-    volumeDecibel = parseFloat(infoPanelVolumes[6].replace('dB)',''));
-    if (volumeDecibel < -8) {
-        volumeDecibel = -8;
-    }
+
+var volumeDecibel = parseFloat(infoPanelVolumes[6].replace('dB)',''));
+// YouTube automatically lowers the volumes of videos louder than 0dBFS to 0dBFS
+if (volumeDecibel > 0) {
+    volumeDecibel = 0;
 }
+
+// *Close the Stats for nerds window
+var infoPanelClose = document.getElementsByClassName("ytp-sfn-close html5-video-info-panel-close ytp-button")[0];
+var click = new MouseEvent("click");
+
+infoPanelClose.dispatchEvent(click);
+
+// Change audio volume using Web Audio API
+var volumeTarget = 1; // 1 is 0dBFS
 
 const audioContext = new AudioContext();
 const audio = document.getElementsByClassName("video-stream html5-main-video")[0];
@@ -27,13 +36,8 @@ const track = audioContext.createMediaElementSource(audio);
 var audioGain = audioContext.createGain();
 
 volumeRatio = 10 ** (volumeDecibel / 20);
-volumeGain = 1 / volumeRatio;
+volumeGain = volumeTarget / volumeRatio;
 
 audioGain.gain.value = volumeGain;
 
 track.connect(audioGain).connect(audioContext.destination);
-
-var infoPanelClose = document.getElementsByClassName("ytp-sfn-close html5-video-info-panel-close ytp-button")[0];
-var click = new MouseEvent("click");
-
-infoPanelClose.dispatchEvent(click);
