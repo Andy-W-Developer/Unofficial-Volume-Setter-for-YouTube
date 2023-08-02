46aa0b87e1b71f10eb1ccac76b0fe272bb8e4621
unknown <128531452+Andy-W-Developer@users.noreply.github.com>
2023-08-03 06:41:41 +1200

    Fix volume target slider range to be smaller

--- src/controls-popup.js
+++ src/controls-popup.js
@@ -3,13 +3,13 @@ localStorage.setItem("volumeTarget", '1')
 
 volumeTargetSlider.addEventListener("input", () => {
     browser.tabs.query({}).then((tabs) => {
-        // 50 is the center of the volume slider, the range is 0 to 100
-        if (volumeTargetSlider.value == 50) {
+        // 20 is the center of the volume slider, the range is 0 to 40
+        if (volumeTargetSlider.value == 20) {
             localStorage.setItem("volumeTarget", '1');
-        } else if (volumeTargetSlider.value < 50) {
-            localStorage.setItem("volumeTarget", (0 + ((1 / 50) * volumeTargetSlider.value)).toString());
+        } else if (volumeTargetSlider.value < 20) {
+            localStorage.setItem("volumeTarget", (0 + ((1 / 20) * volumeTargetSlider.value)).toString());
         } else {
-            volumeTarget = (volumeTargetSlider.value - 49).toString();
+            volumeTarget = (volumeTargetSlider.value - 19).toString();
         }
 
         for (const tab of tabs) {
