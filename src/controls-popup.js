86740d4611adbc3a3bc5e12218dd8c8cce176f41
unknown <128531452+Andy-W-Developer@users.noreply.github.com>
2023-08-03 07:18:15 +1200

    Fix localStorage in background script to variable storage

--- src/controls-popup.js
+++ src/controls-popup.js
@@ -1,19 +1,19 @@
 const volumeTargetSlider = document.getElementById("volume-target-slider");
-localStorage.setItem("volumeTarget", '1')
+var volumeTarget = 1;
 
 volumeTargetSlider.addEventListener("input", () => {
     browser.tabs.query({}).then((tabs) => {
         // 20 is the center of the volume slider, the range is 0 to 40
         if (volumeTargetSlider.value == 20) {
-            localStorage.setItem("volumeTarget", '1');
+            volumeTarget = 1;
         } else if (volumeTargetSlider.value < 20) {
-            localStorage.setItem("volumeTarget", (0 + ((1 / 20) * volumeTargetSlider.value)).toString());
+            volumeTarget = 0 + ((1 / 20) * volumeTargetSlider.value);
         } else {
-            localStorage.setItem("volumeTarget", volumeTargetSlider.value - 19).toString();
+            volumeTarget = volumeTargetSlider.value - 19;
         }
 
         for (const tab of tabs) {
-            browser.tabs.sendMessage(tab.id, {volumeTarget: Integer.parseInt(localStorage.getItem("volumeTarget"))});
+            browser.tabs.sendMessage(tab.id, {volumeTarget: volumeTarget});
         }
     });
 });
