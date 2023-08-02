c7c7b396d6b3240fa59aafccdbe8d2c7f5b9f2e9
unknown <128531452+Andy-W-Developer@users.noreply.github.com>
2023-08-03 06:31:52 +1200

    Add messaging from popup slider value to main script

--- src/controls-popup.js
+++ src/controls-popup.js
@@ -1,9 +1,19 @@
 const volumeTargetSlider = document.getElementById("volume-target-slider");
+volumeTarget = 1;
 
 volumeTargetSlider.addEventListener("input", () => {
     browser.tabs.query({}).then((tabs) => {
+        // 50 is the center of the volume slider, the range is 0 to 100
+        if (volumeTargetSlider.value == 50) {
+            volumeTarget = 1;
+        } else if (volumeTargetSlider.value < 50) {
+            volumeTarget = 0 + ((1 / 50) * volumeTargetSlider.value);
+        } else {
+            volumeTarget = volumeTargetSlider.value - 49;
+        }
+
         for (const tab of tabs) {
-            browser.tabs.sendMessage(tab.id, {volumeTarget: volumeTargetSlider.value});
+            browser.tabs.sendMessage(tab.id, {volumeTarget: volumeTarget});
         }
     });
 });
