31dc1852e281d7267478f9513d06ae42e3e87b1b
unknown <128531452+Andy-W-Developer@users.noreply.github.com>
2023-08-03 05:44:38 +1200

    Remove popup volume meter from script

--- src/controls-popup.js
+++ src/controls-popup.js
@@ -1,10 +1,6 @@
 const volumeTargetSlider = document.getElementById("volume-target-slider");
-const volumeTarget = document.getElementById("volume-target");
-volumeTarget.textContent = volumeTargetSlider.value;
 
 volumeTargetSlider.addEventListener("input", () => {
-    volumeTarget.textContent = volumeTargetSlider.value;
-
     browser.tabs.query({}).then((tabs) => {
         for (const tab of tabs) {
             browser.tabs.sendMessage(tab.id, {volumeTarget: volumeTargetSlider.value});
