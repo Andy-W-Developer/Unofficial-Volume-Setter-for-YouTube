59ff25a4d54421450c7752dbf61879e458a82060
unknown <128531452+Andy-W-Developer@users.noreply.github.com>
2023-08-08 08:36:11 +1200

    Change volume target change running once for every tab to run once

--- src/controls-popup.js
+++ src/controls-popup.js
@@ -8,16 +8,16 @@ browser.runtime.onMessage.addListener((storedVolumeTarget) => {
 });
 
 volumeTargetSlider.addEventListener("input", () => {
-    browser.tabs.query({}).then((tabs) => {
-        // 20 is the center of the volume slider, the range is 0 to 40
-        if (volumeTargetSlider.value == 20) {
-            volumeTarget = 1;
-        } else if (volumeTargetSlider.value < 20) {
-            volumeTarget = 0 + ((1 / 20) * volumeTargetSlider.value);
-        } else {
-            volumeTarget = volumeTargetSlider.value - 19;
-        }
+    // 20 is the center of the volume slider, the range is 0 to 40
+    if (volumeTargetSlider.value == 20) {
+        volumeTarget = 1;
+    } else if (volumeTargetSlider.value < 20) {
+        volumeTarget = 0 + ((1 / 20) * volumeTargetSlider.value);
+    } else {
+        volumeTarget = volumeTargetSlider.value - 19;
+    }
 
+    browser.tabs.query({}).then((tabs) => {
         for (const tab of tabs) {
             browser.tabs.sendMessage(tab.id, {volumeTarget: volumeTarget});
         }
