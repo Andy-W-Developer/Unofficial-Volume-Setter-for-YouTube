050d8b4172ed7d018004272d8a91b1f074ba5771
unknown <128531452+Andy-W-Developer@users.noreply.github.com>
2023-09-05 05:16:33 +1200

    Remove seperate variable for slider value

--- background_scripts/storage.js
+++ background_scripts/storage.js
@@ -1,19 +1,11 @@
 // Send volume target to all tabs on extension startup
 var volumeTarget = localStorage.getItem("volumeTarget");
-var sliderValue = localStorage.getItem("sliderValue");
 
 if (volumeTarget === null) {
     localStorage.setItem("volumeTarget", "1");
     volumeTarget = 1;
 } else {
     volumeTarget = parseFloat(volumeTarget);
-}
-
-if (sliderValue === null) {
-    localStorage.setItem("sliderValue", "20");
-    sliderValue = 20;
-} else {
-    sliderValue = parseInt(sliderValue);
 }
 
 browser.tabs.query({}).then((tabs) => {
@@ -25,8 +17,7 @@ browser.tabs.query({}).then((tabs) => {
 // Send volume target when popup is opened, store volume target when changed in popup
 browser.runtime.onMessage.addListener((newValues) => {
     if (newValues === "popupOpen") {
-        browser.runtime.sendMessage({volumeTarget: parseFloat(localStorage.getItem("volumeTarget")),
-                                     sliderValue: parseInt(localStorage.getItem("sliderValue"))});
+        browser.runtime.sendMessage({volumeTarget: parseFloat(localStorage.getItem("volumeTarget"))});
     } else if (newValues === "injected") {
         browser.tabs.query({}).then((tabs) => {
             for (const tab of tabs) {
@@ -35,6 +26,5 @@ browser.runtime.onMessage.addListener((newValues) => {
         });
     } else {
         localStorage.setItem("volumeTarget", newValues["volumeTarget"]);
-        localStorage.setItem("sliderValue", newValues["sliderValue"]);
     }
 });
--- popup/controls-popup.js
+++ popup/controls-popup.js
@@ -5,21 +5,12 @@ browser.runtime.sendMessage("popupOpen");
 
 browser.runtime.onMessage.addListener((storedValues) => {
     volumeTarget = storedValues["volumeTarget"];
-    volumeTargetSlider.value = storedValues["sliderValue"];
 });
 
 volumeTargetSlider.addEventListener("input", () => {
-    // 20 is the center of the volume slider, the range is 0 to 40
-    if (volumeTargetSlider.value == 20) {
-        volumeTarget = 1;
-    } else if (volumeTargetSlider.value < 20) {
-        volumeTarget = 0 + ((1 / 20) * volumeTargetSlider.value);
-    } else {
-        volumeTarget = volumeTargetSlider.value - 19;
-    }
+    volumeTarget = volumeTargetSlider.value;
 
-    browser.runtime.sendMessage({volumeTarget: volumeTarget.toString(),
-                                 sliderValue: volumeTargetSlider.value.toString()});
+    browser.runtime.sendMessage({volumeTarget: volumeTarget.toString()});
 
     browser.tabs.query({}).then((tabs) => {
         for (const tab of tabs) {
